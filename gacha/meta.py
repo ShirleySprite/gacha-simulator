ed6d533a8ff1daade3631dc1e0196b01c15f1be2
ShirleySprite <583882690@qq.com>
2024-02-05 14:46:52 +0800

    现在`meta`类只存储最基本的信息

--- gacha/meta.py
+++ gacha/meta.py
@@ -1,7 +1,5 @@
-import math
-
-from dataclasses import dataclass, field
-from typing import List, Optional, Union
+from dataclasses import dataclass
+from typing import List, Optional
 
 
 @dataclass
@@ -13,21 +11,6 @@ class GachaMeta:
     prob_increase: Optional[float]
     pity_cnt: int
     official_prob: Optional[float]
-    major_pity: Union[bool, int]
+    major_pity_list: Optional[List]
     refresh: bool
     name: str
-    prob_list: List = field(init=False, repr=False)
-
-    def __post_init__(
-            self
-    ):
-        if self.up_list is None:
-            self.up_list = []
-
-        if type(self.prob_increase) in [int, float]:
-            self.prob_list = [
-                self.base_prob + max(0, i - self.base_cnt) * self.prob_increase
-                for i in range(1, self.pity_cnt + 1)
-            ]
-        else:
-            self.prob_list = []
