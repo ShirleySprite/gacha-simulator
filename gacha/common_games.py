513e1d306be9faba21bad92e6af37f2535df6426
ShirleySprite <583882690@qq.com>
2024-02-05 14:47:03 +0800

    删除了一些不支持的游戏，修改了一些参数

--- gacha/common_games.py
+++ gacha/common_games.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class Games(Enum):
     KLEINS = 1
-    MILKY_WAY = 2
+    # MILKY_WAY = 2
     NOWHERE = 3
     ARKNIGHTS = 4
     THRUD_CHARACTER = 5
@@ -30,20 +30,20 @@ def _g_sys(
             'prob_increase': 0.02,
             'pity_cnt': 80,
             'official_prob': None,
-            'major_pity': True,
+            'major_pity_list': ['A', 'B'],
             'name': 'Kleins'
         },
-        2: {
-            'base_prob': 0.02,
-            'base_cnt': 50,
-            'up_percent': 0.5,
-            'up_list': ['A'],
-            'prob_increase': 0.02,
-            'pity_cnt': 80,
-            'official_prob': None,
-            'major_pity': 150,
-            'name': 'Milky Way'
-        },
+        # 2: {
+        #     'base_prob': 0.02,
+        #     'base_cnt': 50,
+        #     'up_percent': 0.5,
+        #     'up_list': ['A'],
+        #     'prob_increase': 0.02,
+        #     'pity_cnt': 80,
+        #     'official_prob': None,
+        #     'major_pity_list': 150,
+        #     'name': 'Milky Way'
+        # },
         3: {
             'base_prob': 0.02,
             'base_cnt': 50,
@@ -52,7 +52,7 @@ def _g_sys(
             'prob_increase': None,
             'pity_cnt': 80,
             'official_prob': 0.0284,
-            'major_pity': True,
+            'major_pity_list': ['A'],
             'name': 'Nowhere'
         },
         4: {
@@ -63,7 +63,7 @@ def _g_sys(
             'prob_increase': 0.02,
             'pity_cnt': 99,
             'official_prob': None,
-            'major_pity': False,
+            'major_pity_list': None,
             'name': 'Arknights'
         },
         5: {
@@ -74,7 +74,7 @@ def _g_sys(
             'prob_increase': 0.05,
             'pity_cnt': 80,
             'official_prob': 0.018,
-            'major_pity': True,
+            'major_pity_list': ['A'],
             'name': 'Thrud Character'
         },
         6: {
@@ -85,7 +85,7 @@ def _g_sys(
             'prob_increase': 0,
             'pity_cnt': 50,
             'official_prob': 0.025,
-            'major_pity': True,
+            'major_pity_list': ['A'],
             'name': 'Thrud Weapon'
         },
         7: {
@@ -96,7 +96,7 @@ def _g_sys(
             'prob_increase': 0.06,
             'pity_cnt': 90,
             'official_prob': 0.016,
-            'major_pity': True,
+            'major_pity_list': ['A'],
             'name': 'Genshin Impact'
         },
         8: {
@@ -107,7 +107,7 @@ def _g_sys(
             'prob_increase': 0.1,
             'pity_cnt': 70,
             'official_prob': 0.021,
-            'major_pity': True,
+            'major_pity_list': ['A'],
             'name': 'Love and Deepspace'
         }
     }
